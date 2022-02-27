0ad4d082166d7227be64c97a3d115f38201600cf
unknown <id.praxedes@gmail.com>
2022-02-27 17:47:51 -0300

    feat: insert API to GitHub and listing repositories

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,19 +1,23 @@
 import { RepositoryItem } from "./RepositoryItem";
-
-const repository = {
-    name: 'unform',
-    description: 'Form is React',
-    link: 'https://github.com'
-};
+import '../styles/repositories.scss';
+import { useState, useEffect } from "react";
 
 export function RepositoryList(){
+    const [ repositories, setRepositories ] = useState( [] );
+
+    useEffect( () => {
+        fetch('https://api.github.com/users/Alexpraxedes/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, [] );
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem />
-                <RepositoryItem />
+                {repositories.map( repository => (
+                    <RepositoryItem key={repository.name} repository={repository}/>
+                ))}
             </ul>
         </section>
     );
